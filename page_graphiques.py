999b14df7cbc7561f8bf10d121315a725add867e
tilotogo <teo.pujol1804@gmail.com>
2025-10-26 14:41:18 +0100

    implémentation graphiques

---
+++ page_graphiques.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from typing import Tuple
+import pandas as pd
+
+
+class Graphiques:
+    def __init__(self, datas: Tuple[pd.DataFrame, pd.DataFrame]) -> None:
+        self.datas = datas
+
+    def render(self):
+        st.subheader("Deux premiers graphiques")
+        columns = st.columns([0.5, 0.5])
+        for column, data in zip(columns, self.datas):
+            with column:
+                st.bar_chart(data)
+        st.subheader("Deux autres graphiques dont on choisit le contenu")
